cac324de081dc05701db8eff4b1b066a2f395a5f
Sivan Mehta <Sivanmehta2@gmail.com>
2017-10-30 21:26:20 -0700

    [wip] add bouncing stars

--- js/game.js
+++ js/game.js
@@ -12,6 +12,7 @@ function preload() {
 var platforms;
 var player;
 var cursors;
+var stars;
 
 function create() {
   // initialize Physics and world
@@ -33,7 +34,7 @@ function create() {
   game.physics.arcade.enable(player)
 
   player.body.bounce.y = 0.2
-  player.body.gravity.y = 300
+  player.body.gravity.y = 2000
   player.body.collideWorldBounds = true
 
   //  Our two animations, walking left and right.
@@ -41,19 +42,35 @@ function create() {
   player.animations.add('right', [5, 6, 7, 8], 10, true)
 
   cursors = game.input.keyboard.createCursorKeys();
+  stars = game.add.group();
 
+  stars.enableBody = true;
+
+  //  Here we'll create 12 of them evenly spaced apart
+  for (var i = 0; i < 12; i++) {
+      var star = stars.create(i * 70, 0, 'star');
+      star.body.gravity.y = 100;
+      star.body.bounce.y = 0.7 + Math.random() * 0.2;
+  }
+}
+
+function collectStar (player, star) {
+    star.kill();
 }
 
 function update() {
   var hitPlatform = game.physics.arcade.collide(player, platforms);
+  game.physics.arcade.collide(stars, platforms);
+  var collected = game.physics.arcade.overlap(player, stars, collectStar, null, this);
 
   player.body.velocity.x = 0;
+  const travelSpeed = 300
 
   if (cursors.left.isDown) {
-      player.body.velocity.x = -150;
+      player.body.velocity.x = -travelSpeed;
       player.animations.play('left');
   } else if (cursors.right.isDown) {
-    player.body.velocity.x = 150;
+    player.body.velocity.x = travelSpeed;
     player.animations.play('right');
   } else {
     player.animations.stop();
@@ -61,7 +78,7 @@ function update() {
   }
 
   if (cursors.up.isDown && player.body.touching.down && hitPlatform) {
-    player.body.velocity.y = -350;
+    player.body.velocity.y = -1000;
   }
 }
 
